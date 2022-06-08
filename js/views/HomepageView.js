5ed9882dd428e57a4bae901d0d148dbc8d489cea
andreiku <andre@ikuteam.com>
2022-06-08 20:59:55 +0100

    homepage modals done, profile start

--- js/views/HomepageView.js
+++ js/views/HomepageView.js
@@ -1,25 +1,41 @@
-// Get the modal
-const loginModal = document.getElementById("loginModal");
+// Get the modals
+const loginModal = document.getElementById('loginModal');
+const registerModal = document.getElementById('registerModal');
 
+const registerText = document.getElementById('registerText');
 // Get the button that opens the modal
-const loginBtn = document.getElementById("loginButton");
+const loginBtn = document.getElementById('loginButton');
 
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
+const spanLogin = document.getElementsByClassName('close')[0];
+const spanRegister = document.getElementsByClassName('close')[1];
 
 // When the user clicks on the button, open the modal
 loginBtn.onclick = function() {
     loginModal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+registerText.onclick = function() {
+    registerModal.style.display = 'block'
     loginModal.style.display = "none";
+}
+
+// When the user clicks on <span> (x), close the modal
+spanLogin.onclick = function() {
+    loginModal.style.display = "none";
+}
+
+spanRegister.onclick = function() {
+    registerModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == loginModal) {
     loginModal.style.display = "none";
+  }
+
+  if (event.target == registerModal) {
+      registerModal.style.display = "none";
   }
 }
