f35a2ad651f6a922aa56caad81a455495119e582
andreiku <andre@ikuteam.com>
2022-06-18 18:18:46 +0100

    add user

--- js/models/usersModel.js
+++ js/models/usersModel.js
@@ -2,75 +2,85 @@ let users;
 
 // CARREGAR CATs DA LOCALSTORAGE
 export function init() {
-  users = localStorage.users ? JSON.parse(localStorage.users) : [];
+    users = localStorage.users ? JSON.parse(localStorage.users) : [];
 }
 
-export function getAllUsers(){
-
+export function getAllUsers() {
     return users
-
 };
 
 export function removeUser(id) {
     users = users.filter((user) => user.id !== id);
     localStorage.setItem("users", JSON.stringify(users));
-  }
+}
 
-export function editUser(name,password,email,location,gender,birthday,level,experience,blocked,quizzesCompleted) {
+export function addUser(name, password, email, location, gender, birthdate, level, experience, blocked, quizzesCompleted) {
+    if (users.some((user) => user.name === name)) {
+        throw Error(`Theres already an user with the name "${name}"!`);
+    }
+    else {
+        console.log(name);
+        users.push(new User(name, password, email, location, gender, birthdate, level, experience, blocked, quizzesCompleted));
+        localStorage.setItem("users", JSON.stringify(users));
+        console.log(users);
+    }
+}
+
+
+export function editUser(name, password, email, location, gender, birthdate, level, experience, blocked, quizzesCompleted) {
 
     const currentUser = getCurrentUser()
 
     const UserNew = {
-        id:currentUser.id,
-        name:name,
-        password:password,
-        email:email,
-        location:location,
-        gender:gender,
-        birthday:birthday,
-        level:level,
-        experience:experience,
-        blocked:blocked,
-        quizzesCompleted:quizzesCompleted
-
+        id: currentUser.id,
+        name: name,
+        password: password,
+        email: email,
+        location: location,
+        gender: gender,
+        birthdate: birthdate,
+        level: level,
+        experience: experience,
+        blocked: blocked,
+        quizzesCompleted: quizzesCompleted
     }
-    users = users.map(user=>user.id == currentUser.id ? UserNew:user)
+    users = users.map(user => user.id == currentUser.id ? UserNew : user)
     localStorage.setItem('users', JSON.stringify(users));
 
 }
 
 export function setCurrentUser(id) {
     localStorage.setItem("user", id);
-  }
+}
 
 export function getCurrentUser() {
     return users.find((user) => user.id === +localStorage.getItem("user"));
-  }
+}
 
 
 class User {
-    name=""
-    password=""
-    email=""
-    location=""
-    gender=""
-    birthday=""
-    level=1
-    experience=0
-    blocked=false
-    quizzesCompleted=[]
-  
-    constructor(name,password,email,location,gender,birthday,level,experience,blocked,quizzesCompleted) {
-      this.name = name,
-      this.password = password,
-      this.email = email,
-      this.location = location,
-      this.gender = gender,
-      this.birthday = birthday,
-      this.level = level, 
-      this.experience = experience,
-      this.blocked = blocked,
-      this.quizzesCompleted = quizzesCompleted
+    name = ""
+    password = ""
+    email = ""
+    location = ""
+    gender = ""
+    birthday = ""
+    level = 1
+    experience = 0
+    blocked = false
+    quizzesCompleted = []
+
+    constructor(name, password, email, location, gender, birthday, level = 1, experience = 0, blocked = false, quizzesCompleted = []) {
+        this.name = name,
+            this.password = password,
+            this.email = email,
+            this.location = location,
+            this.gender = gender,
+            this.birthday = birthday,
+            this.level = level,
+            this.experience = experience,
+            this.blocked = blocked,
+            this.quizzesCompleted = quizzesCompleted
     }
-  } 
+}
 
--- js/views/HomepageView.js
+++ js/views/HomepageView.js
@@ -1,41 +1,74 @@
+import * as Users from "../models/usersModel.js";
+Users.init();
+
 // Get the modals
 const loginModal = document.getElementById('loginModal');
 const registerModal = document.getElementById('registerModal');
 
 const registerText = document.getElementById('registerText');
 // Get the button that opens the modal
-const loginBtn = document.getElementById('loginButton');
+const loginButtonModal = document.getElementById('loginButtonModal');
+const loginBtn = document.getElementById('loginBtn');
+const registerBtn = document.getElementById('registerBtn');
 
 // Get the <span> element that closes the modal
 const spanLogin = document.getElementsByClassName('close')[0];
 const spanRegister = document.getElementsByClassName('close')[1];
 
+const registerModalForm = document.getElementById('registerModalForm');
+
 // When the user clicks on the button, open the modal
-loginBtn.onclick = function() {
+loginButtonModal.onclick = function () {
     loginModal.style.display = "block";
 }
 
-registerText.onclick = function() {
+registerText.onclick = function () {
     registerModal.style.display = 'block'
     loginModal.style.display = "none";
 }
 
 // When the user clicks on <span> (x), close the modal
-spanLogin.onclick = function() {
+spanLogin.onclick = function () {
     loginModal.style.display = "none";
 }
 
-spanRegister.onclick = function() {
+spanRegister.onclick = function () {
     registerModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == loginModal) {
-    loginModal.style.display = "none";
-  }
+window.onclick = function (event) {
+    if (event.target == loginModal) {
+        loginModal.style.display = "none";
+    }
 
-  if (event.target == registerModal) {
-      registerModal.style.display = "none";
-  }
+    if (event.target == registerModal) {
+        registerModal.style.display = "none";
+    }
 }
+
+registerModalForm.addEventListener('submit', (event) => {
+    try {
+        const username = document.querySelector('#usernameRegister').value;
+        const password = document.querySelector('#passwordRegister').value;
+        const email = document.querySelector('#email').value;
+        const birthdate = document.querySelector('#birthdate').value;
+        const location = document.querySelector('#location').value;
+        const gender = document.querySelector('input[name="gender"]:checked').value
+
+        Users.addUser(
+            username,
+            password,
+            email,
+            location,
+            gender,
+            birthdate
+        )
+    } catch (error) {
+        console.log(error);
+    }
+
+
+    event.preventDefault();
+})
+
